e137f0483a3f4cd053c6ed147349fd9a7e77e174
BethPech <colnze17-94@mail.ru>
2020-11-29 17:31:50 +0300

    CI added

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Product;
 import ru.netology.exception.NotFoundException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductRepositoryTest {
